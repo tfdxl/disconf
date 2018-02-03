019109712a8f368dc9192bc9cf01333e91dcfdb4
tianfeng <tianfengsp>
2018-02-04 00:06:48 +0800

    add

--- disconf/src/main/java/com/monlie/KafkaTest.java
+++ disconf/src/main/java/com/monlie/KafkaTest.java
@@ -2,11 +2,11 @@ package com.monlie;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.producer.ProducerRecord;
 
 import java.util.Properties;
 
-public class KafkaProducerTest {
+public class KafkaTest {
+
     public static void main(String[] args) {
         Properties props = new Properties();
         props.put("bootstrap.servers", "localhost:9092");
@@ -19,9 +19,6 @@ public class KafkaProducerTest {
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
         Producer<String, String> producer = new KafkaProducer<>(props);
-        for (int i = 0; i < 100; i++) {
-            producer.send(new ProducerRecord<>("test", Integer.toString(i), "tianfeng" + Integer.toString(i)));
-        }
-        producer.close();
+        producer.abortTransaction();
     }
 }
