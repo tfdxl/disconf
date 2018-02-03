e66e4812110a5e8ebbdcd0f99f6e3793ea64badf
tianfeng <tianfengsp>
2018-02-03 16:54:46 +0800

    add

---
+++ disconf/src/main/java/com/monlie/App.java
@@ -0,0 +1,13 @@
+package com.monlie;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
