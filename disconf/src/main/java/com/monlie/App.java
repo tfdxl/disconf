550e91f3e4b9ab1d6f9a7b6fe1bb9e7c9b5c7e9a
tianfeng <tianfengsp>
2018-02-03 17:16:17 +0800

    add

--- disconf/src/main/java/com/monlie/App.java
+++ disconf/src/main/java/com/monlie/App.java
@@ -1,10 +1,15 @@
 package com.monlie;
 
 /**
- * Hello world!
+ * java虚拟机会为每一个类型维护一个常量池
  */
 public class App {
     public static void main(String[] args) {
+
+        Object o = new Object();
+        System.err.println(o);
+        boolean e = ("a" + "b" + "c").intern() == "abc";
+        System.err.println(e);
         System.out.println("Hello World!");
     }
 }
